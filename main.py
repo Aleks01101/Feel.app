a61d1fe1394bb2b2e4a06f579f0d81a0396aff4d
AlekseyVolok <57563824+AlekseyVolok@users.noreply.github.com>
2020-09-21 22:21:36 +0200

    added  sign up method

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
-    pass
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
 
 class RootWidget(ScreenManager):
     pass
+
+class SignUpScreen(Screen):
+    def add_user(self,user,password):
+        with open("users.json") as file:
+            users = json.load(file)
+        
+        users[user] = {'username': user, 'password': password, 'created': datetime.now().strftime("%Y-%m-%d time: %H-%M-%S")}
+
+        with open("users.json", 'w') as file:
+            json.dump(users,file)
+        print(users)
 
 class MainApp(App):
     def build(self):
